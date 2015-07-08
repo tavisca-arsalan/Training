73648768e98994eda564442e542bd2a0d4fa03ff
tavisca-arsalan <arsalang@tavisca.com>
2015-07-08 15:12:57 +0530

    Operator Overloading with fixes after review

--- 01 OperatorOverloading/OperatorOverloading.Host/Program.cs
+++ 01 OperatorOverloading/OperatorOverloading.Host/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OperatorOverloading.Model;
+using OperatorOverloading.CustomExceptions;
 
 namespace OperatorOverloading.Host
 {
@@ -15,40 +16,34 @@ namespace OperatorOverloading.Host
             string temporaryCurrency;
 
             //Take the input from user for first money object
-            try{
+            try
+            {
 
-            Console.WriteLine("Enter amount for first money object:");
-            while (double.TryParse(Console.ReadLine(), out  temporaryAmount)==false)
-            {
-                Console.WriteLine("Please enter proper amount for first money object:");
+                Console.WriteLine("Enter amount for first money object:");
+                while (double.TryParse(Console.ReadLine(), out  temporaryAmount) == false)
+                {
+                    Console.WriteLine("Please enter proper amount for first money object:");
+                }
+                Console.WriteLine("Enter currency for first money object:");
+                temporaryCurrency = Console.ReadLine();
+                Money moneyOne = new Money(temporaryAmount, temporaryCurrency);
+
+                //Take the input from user for second money object
+                Console.WriteLine("Enter amount for second money object:");
+                while (double.TryParse(Console.ReadLine(), out  temporaryAmount) == false)
+                {
+                    Console.WriteLine("Please enter proper amount for second money object:");
+                }
+                Console.WriteLine("Enter currency for second money object:");
+                temporaryCurrency = Console.ReadLine();
+                Money moneyTwo = new Money(temporaryAmount, temporaryCurrency);
+
+
+                Money moneyThree = moneyOne + moneyTwo;
+                Console.WriteLine("Third money object is:");
+                Console.WriteLine("Total amount: {0} {1}", moneyThree.Amount, moneyThree.Currency);
             }
-            Console.WriteLine("Enter currency(in upper case) for first money object:");
-            temporaryCurrency = Console.ReadLine();
-            Money moneyOne = new Money(temporaryAmount, temporaryCurrency);
-           
-             //Take the input from user for second money object
-            Console.WriteLine("Enter amount for second money object:");
-            while (double.TryParse(Console.ReadLine(), out  temporaryAmount)==false)
-            {
-                Console.WriteLine("Please enter proper amount for second money object:");
-            }
-            Console.WriteLine("Enter currency(in upper case) for second money object:");
-            temporaryCurrency = Console.ReadLine();
-            Money moneyTwo = new Money(temporaryAmount, temporaryCurrency);
-           
-          
-            Money moneyThree = moneyOne + moneyTwo;
-            Console.WriteLine("Third money object is:");
-            Console.WriteLine("Total amount: {0} {1}", moneyThree.Amount, moneyThree.Currency);
-            }
-            catch (CurrencyMismatchException e) {
-                Console.WriteLine(e.Message);
-            }
-            catch (AmountOverflowException e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            catch (NullReferenceException e)
+            catch (Exception e)
             {
                 Console.WriteLine(e.Message);
             }
